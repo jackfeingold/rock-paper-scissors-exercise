50648242a5f0f0977539e42542bde0cec95f6cf1
jackfeingold <jpf99@georgetown.edu>
2022-01-26 18:09:15 -0500

    Update game.py

--- game.py
+++ game.py
@@ -5,3 +5,22 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+
+# Beginning of code for Rock Paper Scissors Exercise for Jack Feingold OPIM 243
+
+# Welcome message section
+
+# User input section
+
+# Input Validation section
+
+# Computer selection
+
+# Input comparison and outcome
+
+# enter y or n if you would like to play again
+
+# farewell message
+
